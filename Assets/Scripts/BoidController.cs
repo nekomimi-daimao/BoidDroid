f43bbaf4ad3a574af3ff6c017ebc972a94df53c8
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2019-04-06 19:06:49 +0900

    create boid pool

---
+++ Assets/Scripts/BoidController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UniRx;
+using UniRx.Async;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class BoidController : MonoBehaviour
+{
+    private const int DroidCount = 4;
+    private const int Threshold = 1;
+
+    [SerializeField] private BoidDroid _droidState;
+    private BoidsPool _droidPool;
+    private UniTask<Unit> _preload;
+
+    private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+    private BoidDroid[] BoidsChildren;
+
+    private void Awake()
+    {
+        _droidPool = BoidsPool.CreatePool(_droidState, this.transform);
+        _preload = _droidPool.PreloadAsync(DroidCount, Threshold).ToUniTask(_cancellationTokenSource.Token);
+        this.OnDestroyAsObservable().Subscribe((unit =>
+        {
+            _cancellationTokenSource.Cancel();
+            _droidPool.Dispose();
+        }));
+    }
+
+    private void Start()
+    {
+        BoidsChildren = new BoidDroid[DroidCount];
+        for (var count = 0; count < BoidsChildren.Length; count++)
+        {
+            BoidsChildren[count] = _droidPool.Rent();
+        }
+    }
+}
---
+++ Assets/Scripts/BoidsPool.cs
@@ -0,0 +1,25 @@
+﻿using UniRx.Toolkit;
+using UnityEngine;
+
+public class BoidsPool : ObjectPool<BoidDroid>
+{
+    private BoidDroid _prefab;
+    private Transform _parent;
+
+    private BoidsPool(BoidDroid prefab, Transform parent)
+    {
+        _prefab = prefab;
+        _parent = parent;
+    }
+
+    // コンストラクタ隠蔽.趣味.
+    public static BoidsPool CreatePool(BoidDroid prefab, Transform parent)
+    {
+        return new BoidsPool(prefab, parent);
+    }
+
+    protected override BoidDroid CreateInstance()
+    {
+        return GameObject.Instantiate(_prefab, _parent).GetComponent<BoidDroid>();
+    }
+}
