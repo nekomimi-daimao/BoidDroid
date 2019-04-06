0ea2a794df59c7b7e34b69824817f293fb638459
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2019-04-06 13:56:29 +0900

    create ObjectPool

---
+++ Assets/Scripts/DroidPool.cs
@@ -0,0 +1,28 @@
+﻿using UniRx.Toolkit;
+using UnityEngine;
+
+public class DroidPool : ObjectPool<DroidState>
+{
+    private DroidState _prefab;
+    private Transform _parent;
+
+
+    private DroidPool(DroidState prefab, Transform parent)
+    {
+        _prefab = prefab;
+        _parent = parent;
+    }
+
+    // コンストラクタ隠蔽.趣味.
+    static DroidPool CreatePool(DroidState prefab, Transform parent)
+    {
+        return new DroidPool(prefab, parent);
+    }
+
+    protected override DroidState CreateInstance()
+    {
+        return GameObject.Instantiate(_prefab, _parent).GetComponent<DroidState>();
+    }
+    
+    
+}
---
+++ Assets/Scripts/DroidState.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class DroidState : MonoBehaviour
+{
+    [SerializeField] public Rigidbody _rigidbody;
+}
