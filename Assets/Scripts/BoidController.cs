e00af88b045f6812ace48cda153df69c2ae1d5ae
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2019-04-07 21:24:17 +0900

    わからん

--- Assets/Scripts/BoidController.cs
+++ Assets/Scripts/BoidController.cs
@@ -6,22 +6,23 @@ using UniRx.Async;
 using UniRx.Triggers;
 using UnityEngine;
 
-public class BoidController : MonoBehaviour
+public class BoidController : SingletonMonoBehaviour<BoidController>
 {
+    private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
     private const int DroidCount = 4;
     private const int Threshold = 1;
+    private const float Distance = 2.0f;
 
-    [SerializeField] private BoidDroid _droidState;
+    [SerializeField] private BoidDroid _boidPrefab;
     private BoidsPool _droidPool;
     private UniTask<Unit> _preload;
 
-    private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
-
-    private BoidDroid[] BoidsChildren;
+    private readonly List<BoidDroid> _boids = new List<BoidDroid>();
 
     private void Awake()
     {
-        _droidPool = BoidsPool.CreatePool(_droidState, this.transform);
+        _droidPool = BoidsPool.CreatePool(_boidPrefab, this.transform);
         _preload = _droidPool.PreloadAsync(DroidCount, Threshold).ToUniTask(_cancellationTokenSource.Token);
         this.OnDestroyAsObservable().Subscribe((unit =>
         {
@@ -32,10 +33,57 @@ public class BoidController : MonoBehaviour
 
     private void Start()
     {
-        BoidsChildren = new BoidDroid[DroidCount];
-        for (var count = 0; count < BoidsChildren.Length; count++)
+        Ready();
+    }
+
+
+    private void Update()
+    {
+        CalcBoids();
+    }
+
+    public List<BoidDroid> GetNeighbors(Vector2 self)
+    {
+        var list = new List<BoidDroid>();
+        foreach (var boidDroid in _boids)
         {
-            BoidsChildren[count] = _droidPool.Rent();
+            if (boidDroid.PlainPosition() == self)
+            {
+                continue;
+            }
+
+            if ((boidDroid.PlainPosition() - self).sqrMagnitude <= Distance)
+            {
+                list.Add(boidDroid);
+            }
+        }
+
+        return list;
+    }
+
+
+    [SerializeField] private GameObject _center;
+
+    private void CalcBoids()
+    {
+        var center = Vector2.zero;
+        var velocity = Vector3.zero;
+
+        foreach (var boid in _boids)
+        {
+            center += boid.PlainPosition();
+            velocity = boid.Rigidbody.velocity;
+        }
+
+        center = center / DroidCount;
+        velocity = velocity / DroidCount;
+    }
+
+    private void Ready()
+    {
+        for (int count = 0; count < DroidCount; count++)
+        {
+            _boids.Add(_droidPool.Rent());
         }
     }
 }
--- Assets/Scripts/BoidDroid.cs
+++ Assets/Scripts/BoidDroid.cs
@@ -4,15 +4,98 @@ using UnityEngine;
 
 public class BoidDroid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public Rigidbody Rigidbody;
+
+    public Vector2 PlainPosition()
     {
-        
+        return new Vector2(transform.position.x, transform.position.z);
     }
 
-    // Update is called once per frame
+    public Vector2 PlainVelocity()
+    {
+        return new Vector2(Rigidbody.velocity.x, Rigidbody.velocity.z);
+    }
+
+    private Vector2 velocity;
+    private Vector2 accel;
+    private Vector2 pos;
+    private const float min = 0.1f;
+    private const float max = 1f;
+
+
     void Update()
     {
-        
+        var dt = Time.deltaTime;
+
+        velocity += accel * dt;
+        var dir = velocity.normalized;
+        var speed = velocity.magnitude;
+        velocity = Mathf.Clamp(speed, min, max) * dir;
+        pos += velocity * dt;
+
+        var rot = Quaternion.LookRotation(new Vector3(velocity.x, 0, velocity.y));
+        transform.SetPositionAndRotation(pos, rot);
+
+        accel = Vector3.zero;
+    }
+
+    void OnEnable()
+    {
+        transform.position = new Vector3(Random.value, Random.value, Random.value);
+    }
+
+
+    private Vector2 Rule1()
+    {
+        var where = PlainPosition();
+        var neighbors = BoidController.Instance.GetNeighbors(where);
+        if (neighbors.Count == 0)
+        {
+            return Vector2.zero;
+        }
+
+        var vec = Vector2.zero;
+        foreach (var boidDroid in neighbors)
+        {
+            var diff = boidDroid.PlainPosition() - where;
+            vec += -1 * diff.normalized * 10.0f / (diff.sqrMagnitude);
+        }
+
+        return vec / neighbors.Count;
+    }
+
+    private Vector2 Rule2()
+    {
+        var neighbors = BoidController.Instance.GetNeighbors(PlainPosition());
+
+        if (neighbors.Count == 0)
+        {
+            return Vector2.zero;
+        }
+
+        var vec = Vector2.zero;
+        foreach (var boidDroid in neighbors)
+        {
+            vec += boidDroid.PlainVelocity();
+        }
+
+        return (vec / neighbors.Count) - PlainVelocity();
+    }
+
+    private Vector2 Rule3()
+    {
+        var neighbors = BoidController.Instance.GetNeighbors(PlainPosition());
+
+        if (neighbors.Count == 0)
+            return Vector2.zero;
+
+        var pos = Vector2.zero;
+
+        foreach (var boidDroid in neighbors)
+        {
+            pos += boidDroid.PlainPosition();
+        }
+
+        return (pos / neighbors.Count) - PlainPosition();
     }
 }
